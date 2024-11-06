3a034bff091fd79cb23b668ac04738b3ae4c0ebb
S-M-S-A (S_Shah) <90388189+S-SHAHH@users.noreply.github.com>
2024-11-06 11:37:56 +0100

    Add files via upload
    src files for the Music Playlist  React/JS/CSS/Html

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./App.css";
+import { useState } from "react";
+// import ReactAudioPlayer from "react-audio-player";
+import $ from "jquery";
+// import audio from "react";
+
+function App() {
+  const [sList, updSong] = useState({
+    songName: "Save Ur tears",
+    sors: "./src/songs/01.mp3",
+    img: "",
+  });
+
+  // $(".wndw1").on("load", function () {
+  //   $(".wndw1").css({
+  //     backgroundColor: "black",
+  //     width: "800px",
+  //     height: "700px",
+  //   });
+  // });
+
+  return (
+    <div className="App">
+      <div className="wndw1">
+        <button
+          id="closbtn"
+          onClick={() => {
+            $(".wndw1").hide();
+          }}
+        >
+          X
+        </button>
+        <div className="sWndw">
+          <h3>{sList.songName}</h3>
+
+          {/* <ul> <li>{/* <a href="#" /></li> </ul> */}
+        </div>
+        <div id="aud">
+          <audio src={sList.sors} title={sList.songName} type="audio/mp3" autoplay controls />
+        </div>
+        <div>
+          <button
+            id="nxtbtn"
+            onClick={e => {
+              updSong({
+                songName: e.target.songName,
+                // src: /              )
+              });
+            }}
+          >
+            Nxt_S
+          </button>
+        </div>
+        <div id="sList">
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+          <div id="slitms">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
